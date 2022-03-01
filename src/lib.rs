d2a5dfa7ac25876521585d481f10e0e78954dbb8
drakobian <drake.bennion@gmail.com>
2022-02-28 21:53:30 -0700

    feat: reserve some memory for interpreter

--- src/lib.rs
+++ src/lib.rs
@@ -114,14 +114,129 @@ impl CPUBuilder {
     ///                         .build();
     /// ```
     pub fn build(&self) -> CPU {
+        // todo: update memory to reserve 0x000 to 0x1FF for interpreter
+        // and store some character sprites
+        let memory = self.get_memory();
+
         CPU {
-            program_counter: 0,
+            program_counter: 200,
             registers: self.registers.unwrap_or([0; 16]),
-            memory: self.memory.unwrap_or([0; 0x1000]),
+            memory,
             stack: [0; 16],
             stack_pointer: 0,
             i: 0,
         }
+    }
+
+    // todo: pull the reserved characters into a separate file
+    fn get_memory(&self) -> Memory {
+        let mut memory = [0; 0x1000];
+
+        // populate memory w/ reserved characters
+        memory[0] = 0xF0;
+        memory[1] = 0x90;
+        memory[2] = 0x90;
+        memory[3] = 0x90;
+        memory[4] = 0xF0;
+
+        memory[5] = 0x20;
+        memory[6] = 0x60;
+        memory[7] = 0x20;
+        memory[8] = 0x20;
+        memory[9] = 0x70;
+
+        memory[10] = 0xF0;
+        memory[11] = 0x10;
+        memory[12] = 0xF0;
+        memory[13] = 0x80;
+        memory[14] = 0xF0;
+
+        memory[15] = 0xF0;
+        memory[16] = 0x10;
+        memory[17] = 0xF0;
+        memory[18] = 0x10;
+        memory[19] = 0xF0;
+
+        memory[20] = 0x90;
+        memory[21] = 0x90;
+        memory[22] = 0xF0;
+        memory[23] = 0x10;
+        memory[24] = 0x10;
+
+        memory[25] = 0xF0;
+        memory[26] = 0x80;
+        memory[27] = 0xF0;
+        memory[28] = 0x10;
+        memory[29] = 0xF0;
+
+        memory[30] = 0xF0;
+        memory[31] = 0x80;
+        memory[32] = 0xF0;
+        memory[33] = 0x90;
+        memory[34] = 0xF0;
+
+        memory[35] = 0xF0;
+        memory[36] = 0x10;
+        memory[37] = 0x20;
+        memory[38] = 0x40;
+        memory[39] = 0x40;      
+
+        memory[40] = 0xF0;
+        memory[41] = 0x90;
+        memory[42] = 0xF0;
+        memory[43] = 0x90;
+        memory[44] = 0xF0;
+
+        memory[45] = 0xF0;
+        memory[46] = 0x90;
+        memory[47] = 0xF0;
+        memory[48] = 0x10;
+        memory[49] = 0xF0;
+
+        memory[50] = 0xF0;
+        memory[51] = 0x90;
+        memory[52] = 0xF0;
+        memory[53] = 0x90;
+        memory[54] = 0x90;
+
+        memory[55] = 0xE0;
+        memory[56] = 0x90;
+        memory[57] = 0xE0;
+        memory[58] = 0x90;
+        memory[59] = 0xE0;
+
+        memory[60] = 0xF0;
+        memory[61] = 0x80;
+        memory[62] = 0x80;
+        memory[63] = 0x80;
+        memory[64] = 0xF0;
+
+        memory[65] = 0xE0;
+        memory[66] = 0x90;
+        memory[67] = 0x90;
+        memory[68] = 0x90;
+        memory[69] = 0xE0;
+
+        memory[70] = 0xF0;
+        memory[71] = 0x80;
+        memory[72] = 0xF0;
+        memory[73] = 0x80;
+        memory[74] = 0xF0;
+
+        memory[75] = 0xF0;
+        memory[76] = 0x80;
+        memory[77] = 0xF0;
+        memory[78] = 0x80;
+        memory[79] = 0x80;
+
+        // some interpreter memory is open :)
+
+        // populate rest of memory if any memory was passed in
+        for i in 200..4096 {
+            memory[i] = self.memory.unwrap_or([0; 4096])[i - 200];
+        }
+
+        memory
     }
 }
 
@@ -228,7 +343,6 @@ impl CPU {
 
         for i in 0..(d as usize) {
             let byte = self.memory[self.i as usize + i];
-
             bits.push(format!("{:b}", byte));
         }
 
